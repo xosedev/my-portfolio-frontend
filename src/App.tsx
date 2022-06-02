cfc5be7c9c28b2d922beefc6d8c2233b75e4707c
José Leonado Toro <86394659+xosedev@users.noreply.github.com>
2022-06-02 00:02:10 -0400

    create folder basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/navbar/navbar.component';
 
 function App() {
   return (
     <div className="App">
-      Site in development
+      <NavBar/>
     </div>
   );
 }
