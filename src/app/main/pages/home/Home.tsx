82ed544e474dc6eab31b02f47f15b69685fa364d
José Leonado Toro <86394659+xosedev@users.noreply.github.com>
2022-07-06 21:29:02 -0400

    feature/add cv button to homr

--- src/app/main/pages/home/Home.tsx
+++ src/app/main/pages/home/Home.tsx
@@ -43,7 +43,7 @@ function HomePage() {
                           </div>
                           <div className="mt-3 sm:mt-0 sm:ml-3">
                             <a
-                              href="https://www.xosedev.com/.uploads/docs/cv/JOSE_%20LEONARDO%20TORO%20MONTECINOS%20-%20CV.pdf" target={'_blank'} 
+                              href="https://www.xosedev.com/.uploads/docs/cv/JOSE_%20LEONARDO%20TORO%20MONTECINOS%20-%20CV.pdf" target={'_blank'}  rel="noreferrer"
                               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                                download>
                               CV
