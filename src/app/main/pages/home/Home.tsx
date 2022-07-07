869c6825118674f52e580a1ae1984de11fa7f02f
José Leonado Toro <86394659+xosedev@users.noreply.github.com>
2022-07-06 21:26:10 -0400

    feature/add cv button to home

--- src/app/main/pages/home/Home.tsx
+++ src/app/main/pages/home/Home.tsx
@@ -5,23 +5,12 @@ import emoji from "react-easy-emoji";
 import "./style.css";
 import { useSelector } from 'react-redux';
 import { selectUser } from '../../../store/userSlice';
-import axios from 'axios';
-import fileDownload from 'js-file-download'
-import { Button } from '@mui/material';
 const Root = styled(StructurePage)(({ theme }) => ({
 }));
 
 function HomePage() {
   const { t } = useTranslation('homePage');
   const user = useSelector(selectUser);
-
-  const handleDownload = (url: string, filename: string) => {
-    axios.get(url)
-      .then((res) => {
-        fileDownload(res.data, filename)
-      })
-  }
-
   return (
     <Root content={
       <main>
@@ -53,14 +42,12 @@ function HomePage() {
                             </a>
                           </div>
                           <div className="mt-3 sm:mt-0 sm:ml-3">
-                            <Button
-                              onClick={() => {
-                                handleDownload('https://www.xosedev.com/.uploads/docs/cv/JOSE_%20LEONARDO%20TORO%20MONTECINOS%20-%20CV.pdf', 'JOSE_TORO_CV.pdf')
-                              }}
+                            <a
+                              href="https://www.xosedev.com/.uploads/docs/cv/JOSE_%20LEONARDO%20TORO%20MONTECINOS%20-%20CV.pdf" target={'_blank'} 
                               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-                            >
+                               download>
                               CV
-                            </Button>
+                            </a>
                           </div>
                         </div>
                       </div>
@@ -68,11 +55,13 @@ function HomePage() {
                   </div>
                 </div>
                 <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 flex  justify-center ">
-                  <img
-                    className="scale-0 h-56 sm:h-50 md:h-40 lg:w-80 lg:h-80 lg:mt-40 rounded-full shadow-lg"
+             
+                  <img 
+                    className="scale-0 h-56 sm:h-50 md:h-40 lg:w-80 lg:h-80 lg:mt-40 rounded-full shadow-lg shadow-lg"
                     src={user.img}
                     alt=""
                   />
+              
                 </div>
               </div>
             </div>
